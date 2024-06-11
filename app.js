1a82ce11f53346e5ad4b445cbe873db0dd88c426
Dulithi Jayasooriya <129371864+Dulithi@users.noreply.github.com>
2024-06-11 14:35:29 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,91 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const _ = require("lodash");
+// const { render } = require("ejs");
+const { default: mongoose } = require("mongoose");
+// const ejs = require("ejs");
+require('dotenv').config();
+
+const USER_NAME = process.env.DATABASE_USERNAME;
+const PASSWORD = process.env.DATABASE_PASSWORD;
+const port = process.env.PORT;
+
+const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
+const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
+const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+app.locals._ = _;
+
+const uri = `mongodb+srv://${USER_NAME}:${PASSWORD}@cluster0.qrml7jb.mongodb.net/blogWebsiteDB?retryWrites=true&w=majority&appName=Cluster0`;
+
+mongoose.connect(uri);
+
+const postSchema = new mongoose.Schema({
+  title: String,
+  content: String
+})
+
+const Post = mongoose.model("Post", postSchema);
+
+
+app.get("/", async (req, res) => {
+
+  const posts = await Post.find({});
+  
+  res.render("home", {
+    homeContent: homeStartingContent, 
+    posts: posts
+  });
+})
+
+app.get("/about", (req, res) => {
+  res.render("about", {aboutContent: aboutContent});
+})
+
+app.get("/contact", (req, res) => {
+  res.render("contact", {contactContent: contactContent});
+})
+
+app.get("/compose", (req, res) => {
+  res.render("compose");
+})
+
+app.post("/compose", (req, res) => {
+  const post = new Post({
+    title : _.startCase(req.body.postTitle),
+    content: req.body.postContent,
+  });
+
+  post.save();
+  res.redirect("/");
+
+})
+
+app.get("/posts/:postName",async (req, res) => {
+
+  const requestedTitle = req.params.postName;
+  const match = await Post.findOne({title : _.startCase(requestedTitle)});
+
+  // const match = posts.find((post) => {
+  //   return  _.toLower(post.title) === _.lowerCase(requestedTitle);
+  // });
+
+  if(match) {
+    console.log("Match found");
+    res.render("post", {title: match.title, content: match.content});
+  } else {
+    console.log("Match not found");
+  }
+  
+})
+
+app.listen(port, ()=> {
+  console.log(`Server running at port ${port}`);
+})
